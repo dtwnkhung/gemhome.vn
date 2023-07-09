1a96b572c300a78ef1b0c9227b7214df66d1c3b2
dtwnkhung <quangnhat249@gmail.com>
2023-07-10 00:25:57 +0700

    Update UI

--- resources/views/layouts/apex_page.blade.php
+++ resources/views/layouts/apex_page.blade.php
@@ -113,6 +113,7 @@
     href='/wp-content/themes/dor/assets/css/modules-responsive.min.css?ver=5.8.3' type='text/css' media='all' />
   <link rel='stylesheet' id='dor-mikado-style-dynamic-responsive-css'
     href='/wp-content/themes/dor/assets/css/style_dynamic_responsive.css?ver=1642768118' type='text/css' media='all' />
+    <link href="https://rawcdn.githack.com/hung1001/font-awesome-pro/4cac1a6/css/all.css" rel="stylesheet" type="text/css" />
   <style id="dor-mikado-google-fonts-css" media="all">
     /* cyrillic-ext */
     @font-face {
@@ -2213,6 +2214,12 @@
     type='text/css' media='all' />
   <script type='text/javascript' src='https://dor.qodeinteractive.com/wp-includes/js/jquery/jquery.min.js?ver=3.6.0'
     id='jquery-core-js'></script>
+  <script>
+    const currentPath = location.pathname
+    if(currentPath === "/"){
+      document.title = "GEM";
+    }
+  </script>
   <script type='text/javascript'
     src='https://dor.qodeinteractive.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.3.2'
     id='jquery-migrate-js'></script>
@@ -2285,6 +2292,184 @@
   <link rel="icon" href="/wp-content/uploads/2019/05/cropped-Dor-favicon-1-192x192.png" sizes="192x192" />
   <link rel="apple-touch-icon" href="/wp-content/uploads/2019/05/cropped-Dor-favicon-1-180x180.png" />
   <meta name="msapplication-TileImage" content="/wp-content/uploads/2019/05/cropped-Dor-favicon-1-270x270.png" />
+  <style type="text/css">
+    .hotline-phone-ring-wrap {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 999999;
+}
+.hotline-phone-ring {
+  position: relative;
+  visibility: visible;
+  background-color: transparent;
+  width: 110px;
+  height: 110px;
+  cursor: pointer;
+  z-index: 11;
+  -webkit-backface-visibility: hidden;
+  -webkit-transform: translateZ(0);
+  transition: visibility .5s;
+  left: 0;
+  bottom: 0;
+  display: block;
+}
+.hotline-phone-ring-circle {
+  width: 85px;
+  height: 85px;
+  top: 10px;
+  left: 10px;
+  position: absolute;
+  background-color: transparent;
+  border-radius: 100%;
+  border: 2px solid #e60808;
+  -webkit-animation: phonering-alo-circle-anim 1.2s infinite ease-in-out;
+  animation: phonering-alo-circle-anim 1.2s infinite ease-in-out;
+  transition: all .5s;
+  -webkit-transform-origin: 50% 50%;
+  -ms-transform-origin: 50% 50%;
+  transform-origin: 50% 50%;
+  opacity: 0.5;
+}
+.hotline-phone-ring-circle-fill {
+  width: 55px;
+  height: 55px;
+  top: 25px;
+  left: 25px;
+  position: absolute;
+  background-color: rgba(230, 8, 8, 0.7);
+  border-radius: 100%;
+  border: 2px solid transparent;
+  -webkit-animation: phonering-alo-circle-fill-anim 2.3s infinite ease-in-out;
+  animation: phonering-alo-circle-fill-anim 2.3s infinite ease-in-out;
+  transition: all .5s;
+  -webkit-transform-origin: 50% 50%;
+  -ms-transform-origin: 50% 50%;
+  transform-origin: 50% 50%;
+}
+.hotline-phone-ring-img-circle {
+  background-color: #e60808;
+  width: 33px;
+  height: 33px;
+  top: 37px;
+  left: 37px;
+  position: absolute;
+  background-size: 20px;
+  border-radius: 100%;
+  border: 2px solid transparent;
+  -webkit-animation: phonering-alo-circle-img-anim 1s infinite ease-in-out;
+  animation: phonering-alo-circle-img-anim 1s infinite ease-in-out;
+  -webkit-transform-origin: 50% 50%;
+  -ms-transform-origin: 50% 50%;
+  transform-origin: 50% 50%;
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.hotline-phone-ring-img-circle .pps-btn-img {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+}
+.hotline-phone-ring-img-circle .pps-btn-img img {
+  width: 20px;
+  height: 20px;
+}
+.hotline-bar {
+  position: absolute;
+  background: rgba(230, 8, 8, 0.75);
+  height: 40px;
+  width: 180px;
+  line-height: 40px;
+  border-radius: 3px;
+  padding: 0 10px;
+  background-size: 100%;
+  cursor: pointer;
+  transition: all 0.8s;
+  -webkit-transition: all 0.8s;
+  z-index: 9;
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 50px !important;
+  /* width: 175px !important; */
+  left: 33px;
+  bottom: 37px;
+}
+.hotline-bar > a {
+  color: #fff;
+  text-decoration: none;
+  font-size: 15px;
+  font-weight: bold;
+  text-indent: 50px;
+  display: block;
+  letter-spacing: 1px;
+  line-height: 40px;
+  font-family: Arial;
+}
+.hotline-bar > a:hover,
+.hotline-bar > a:active {
+  color: #fff;
+}
+@-webkit-keyframes phonering-alo-circle-anim {
+  0% {
+    -webkit-transform: rotate(0) scale(0.5) skew(1deg);
+    -webkit-opacity: 0.1;
+  }
+  30% {
+    -webkit-transform: rotate(0) scale(0.7) skew(1deg);
+    -webkit-opacity: 0.5;
+  }
+  100% {
+    -webkit-transform: rotate(0) scale(1) skew(1deg);
+    -webkit-opacity: 0.1;
+  }
+}
+@-webkit-keyframes phonering-alo-circle-fill-anim {
+  0% {
+    -webkit-transform: rotate(0) scale(0.7) skew(1deg);
+    opacity: 0.6;
+  }
+  50% {
+    -webkit-transform: rotate(0) scale(1) skew(1deg);
+    opacity: 0.6;
+  }
+  100% {
+    -webkit-transform: rotate(0) scale(0.7) skew(1deg);
+    opacity: 0.6;
+  }
+}
+@-webkit-keyframes phonering-alo-circle-img-anim {
+  0% {
+    -webkit-transform: rotate(0) scale(1) skew(1deg);
+  }
+  10% {
+    -webkit-transform: rotate(-25deg) scale(1) skew(1deg);
+  }
+  20% {
+    -webkit-transform: rotate(25deg) scale(1) skew(1deg);
+  }
+  30% {
+    -webkit-transform: rotate(-25deg) scale(1) skew(1deg);
+  }
+  40% {
+    -webkit-transform: rotate(25deg) scale(1) skew(1deg);
+  }
+  50% {
+    -webkit-transform: rotate(0) scale(1) skew(1deg);
+  }
+  100% {
+    -webkit-transform: rotate(0) scale(1) skew(1deg);
+  }
+}
+@media (max-width: 768px) {
+  .hotline-bar {
+    display: none;
+  }
+}
+  </style>
   <script>function setREVStartSize(e) {
       //window.requestAnimationFrame(function() {
       window.RSIW = window.RSIW === undefined ? window.innerWidth : window.RSIW;
@@ -2479,7 +2664,32 @@
             </div>
             <div class="mkdf-position-right">
               <div class="mkdf-position-right-inner">
-                <!--[socials_link]-->
+                <div style="
+                display: flex;
+                align-items: center;
+                justify-content:center;
+                ">
+                  <a style="
+                  color: #fff;
+                  font-size:16px;
+                  margin-right: 15px;
+                  text-decoration: none;" href="https://www.facebook.com/Gem Architecture" target="_blank">
+                    <i class="fab fa-facebook"></i>
+                  </a>
+                  <a style="
+                  color: #fff;
+                  font-size:16px;
+                  margin-right: 15px;
+                  text-decoration: none;" href="https://www.instagram.com/gem.architecture/" target="_blank">
+                    <i class="fab fa-instagram-square"></i>
+                  </a>
+                  <a style="
+                  color: #fff;
+                  font-size:16px;
+                  text-decoration: none;" href="#" target="_blank">
+                    <i class="fab fa-youtube"></i>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
@@ -2504,7 +2714,34 @@
               </div>
               <div class="mkdf-position-right">
                 <div class="mkdf-position-right-inner">
-                  <!--[socials_link]-->
+                  <div class="mkdf-position-right-inner">
+                <div style="
+                display: flex;
+                align-items: center;
+                justify-content:center;
+                ">
+                  <a style="
+                  color: #fff;
+                  font-size:16px;
+                  margin-right: 15px;
+                  text-decoration: none;" href="https://www.facebook.com/Gem Architecture" target="_blank">
+                    <i class="fab fa-facebook"></i>
+                  </a>
+                  <a style="
+                  color: #fff;
+                  font-size:16px;
+                  margin-right: 15px;
+                  text-decoration: none;" href="https://www.instagram.com/gem.architecture/" target="_blank">
+                    <i class="fab fa-instagram-square"></i>
+                  </a>
+                  <a style="
+                  color: #fff;
+                  font-size:16px;
+                  text-decoration: none;" href="#" target="_blank">
+                    <i class="fab fa-youtube"></i>
+                  </a>
+                </div>
+              </div>
                 </div>
               </div>
             </div>
@@ -2574,9 +2811,24 @@
     @isset($footer_content)
         {!! $footer_content !!}
     @endisset  
-
     </div>
   </div>
+  <div class="hotline-phone-ring-wrap">
+  <div class="hotline-phone-ring">
+    <div class="hotline-phone-ring-circle"></div>
+    <div class="hotline-phone-ring-circle-fill"></div>
+    <div class="hotline-phone-ring-img-circle">
+    <a href="tel:0915550991" class="pps-btn-img">
+      <img src="https://nguyenhung.net/wp-content/uploads/2019/05/icon-call-nh.png" alt="Gọi điện thoại" width="50">
+    </a>
+    </div>
+  </div>
+  <div class="hotline-bar">
+    <a href="tel:0915550991">
+      <span class="text-hotline">0915.550.991</span>
+    </a>
+  </div>
+</div>
   <section class="mkdf-side-menu">
     <a class="mkdf-close-side-menu mkdf-close-side-menu-svg-path" href="#">
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px"
